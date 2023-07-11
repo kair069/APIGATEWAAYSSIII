b2ea42c1a8bd0eb422e2bf27783d262aa1364717
kair069 <81661845+kair069@users.noreply.github.com>
2023-07-11 15:11:44 -0500

    Add files via upload

---
+++ src/test/java/com/example/apigateawy/APigateawyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigateawy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APigateawyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
